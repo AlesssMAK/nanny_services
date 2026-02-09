0c062aec8027e96515e4055b44c2865600324f26
Alexey <makovii88@gmail.com>
2026-02-10 00:11:59 +0100

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+// import css from './App.module.css';
+import Home from '../../pages/Home/Home';
+import Nannies from '../../pages/Nannies/Nannies';
+import Favorites from '../../pages/Favorites/Favorites';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/nannies" element={<Nannies />} />
+      <Route path="/favorites" element={<Favorites />} />
+    </Routes>
+  );
+}
+
+export default App;
