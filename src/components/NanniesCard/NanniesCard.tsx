d8db9b6a8467e08096ba2df56023c50677557519
Alexey <makovii88@gmail.com>
2026-02-22 15:13:27 +0100

    add nanny card

--- src/components/NanniesCard/NanniesCard.tsx
+++ src/components/NanniesCard/NanniesCard.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import type { Nanny } from '../../types/nanny';
 import css from './NanniesCard.module.css';
+import Button from '../UI/Button/Button';
 
 interface NanniesCardProps {
   nanny: Nanny;
 }
 
 const NanniesCard = ({ nanny }: NanniesCardProps) => {
+  const [isShow, setIsShow] = useState(false);
   console.log(nanny);
+
+  const calcAge = (birthday: string) => {
+    const birthdayDate = new Date(birthday);
+    const today = new Date();
+    let age = today.getFullYear() - birthdayDate.getFullYear();
+    const month = today.getMonth() - birthdayDate.getMonth();
+    if (month < 0 || (month === 0 && today.getDate() < birthdayDate.getDate()))
+      age--;
+    return age;
+  };
 
   return (
     <div className={css.nanny_container}>
@@ -19,6 +32,8 @@ const NanniesCard = ({ nanny }: NanniesCardProps) => {
           className={css.img}
         />
       </div>
+
+      {/* ------- info ------- */}
       <div className={css.card_container}>
         <div className={css.name_info_container}>
           <div className={css.name_container}>
@@ -68,6 +83,100 @@ const NanniesCard = ({ nanny }: NanniesCardProps) => {
             </button>
           </div>
         </div>
+
+        {/* ------- profile ------- */}
+        <ul className={css.profile_list}>
+          <li className={css.profile_item_list}>
+            <p className={css.profile_item_text}>
+              Age:{' '}
+              <span
+                className={`${css.profile_item_details} ${css.profile_item_age_details}`}
+              >
+                {calcAge(nanny.birthday)}
+              </span>
+            </p>
+          </li>
+          <li className={css.profile_item_list}>
+            <p className={css.profile_item_text}>
+              Experience:{' '}
+              <span className={css.profile_item_details}>
+                {nanny.experience}
+              </span>
+            </p>
+          </li>
+          <li className={css.profile_item_list}>
+            <p className={css.profile_item_text}>
+              Kids Age:{' '}
+              <span className={css.profile_item_details}>{nanny.kids_age}</span>
+            </p>
+          </li>
+          <li className={css.profile_item_list}>
+            <p className={css.profile_item_text}>
+              Characters:{' '}
+              <span className={css.profile_item_details}>
+                {nanny.characters.join(', ')}
+              </span>
+            </p>
+          </li>
+          <li className={css.profile_item_list}>
+            <p className={css.profile_item_text}>
+              Education:{' '}
+              <span className={css.profile_item_details}>
+                {nanny.education}
+              </span>
+            </p>
+          </li>
+        </ul>
+
+        {/* ------- about ------- */}
+        <p className={`${css.about_text} ${isShow ? css.add_margin : ''}`}>
+          {nanny.about}
+        </p>
+
+        {/* ------- reviews ------- */}
+        <button
+          type="button"
+          onClick={() => {
+            setIsShow(true);
+          }}
+          className={`${css.read_more_btn} ${isShow ? css.hidden : ''}`}
+        >
+          Read more
+        </button>
+
+        {isShow && (
+          <div className={css.reviews_block_container}>
+            <div className={css.reviews_container}>
+              {nanny.reviews.map(review => (
+                <div className={css.review_container}>
+                  <div className={css.reviewer_container}>
+                    <div className={css.reviewer_avatar}>
+                      {review.reviewer.charAt(0).toUpperCase()}
+                    </div>
+                    <div className={css.user_review_container}>
+                      <p className={css.reviewer_name}>{review.reviewer}</p>
+                      <div className={css.review_rating_container}>
+                        <svg
+                          width="16"
+                          height="26"
+                          className={css.review_rating_icon}
+                          aria-label="star"
+                        >
+                          <use href="sprite.svg#star"></use>
+                        </svg>
+                        <p className={css.reviewer_rating}>{review.rating}</p>
+                      </div>
+                    </div>
+                  </div>
+                  <p className={css.reviewer_comment}>{review.comment}</p>
+                </div>
+              ))}
+            </div>
+            <Button type="button" className="button" width={215}>
+              Make an appointment
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
