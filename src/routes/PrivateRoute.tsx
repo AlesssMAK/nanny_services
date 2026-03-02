b68bbf9d0af8d9a7ce9ba356848b15cdb48c528e
Alexey <makovii88@gmail.com>
2026-03-02 12:55:27 +0100

    fix errors for deploy

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,12 +1,18 @@
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/Auth/useAuth';
+import Loader from '../components/Loader/Loader';
 
 interface PrivateRouteProps {
   children: React.ReactNode;
 }
 
 export default function PrivateRoute({ children }: PrivateRouteProps) {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, loading } = useAuth();
+
+  if (loading) {
+    <Loader />;
+    return null;
+  }
   if (!isAuthenticated) return <Navigate to="/" replace />;
   return children;
 }
